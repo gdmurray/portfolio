4e575409f00c9e737d123f4f827eb9941fa56db0
gregmurray <gd-murray@hotmail.com>
2019-05-30 21:02:59 -0400

    basic push

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
+import React, {Component} from 'react';
+import { Route, Switch, withRouter} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import Home from './Home';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  render(){
+    return (
+      <div>
+        <Route exact path="/" component={Home}/>  
+      </div>
+    )
+  }
 }
 
-export default App;
+export default withRouter(App);
